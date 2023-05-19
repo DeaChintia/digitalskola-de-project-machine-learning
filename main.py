b43bb6f38757c4b392bdd8d3441b1b01d5a73a31
Dea Chintia Putri <dea.chintiaputri@gmail.com>
2023-05-19 15:30:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from flask import Flask
+from flask import request
+from flask import jsonify
+from modules.insurance_model import InsuranceModel
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "API Modellings"
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame(data, index=[0])
+    x = InsuranceModel().runModel(df.head(1), typed='single')
+
+    return jsonify({
+        "status": "Predicted",
+        "predict_result": x
+    })
